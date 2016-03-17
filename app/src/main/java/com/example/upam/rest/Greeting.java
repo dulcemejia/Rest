38f4468fa7107a0ea8102e8736115dc9b01f6ab7
NanoCodeInc <nanocode.inc@gmail.com>
2016-03-17 13:30:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/upam/rest/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.upam.rest;
+
+/**
+ * Created by upam on 17/03/16.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
